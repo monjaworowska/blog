600dc056ac7be3cb1e6a2abbcc51274c9d1859e5
Monika Jaworowska <mon.jaworowska@gmail.com>
2021-03-14 17:17:44 +0100

    Post component done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,52 @@
-const App = () => <h1 className="notification is-info">App</h1>;
+import { BrowserRouter, Switch } from "react-router-dom";
+import Post from "./component/Post";
+
+const post = {
+  userId: 1,
+  id: 1,
+  title:
+    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  body:
+    "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+};
+
+const user = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    geo: {
+      lat: "-37.3159",
+      lng: "81.1496",
+    },
+  },
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+  company: {
+    name: "Romaguera-Crona",
+    catchPhrase: "Multi-layered client-server neural-net",
+    bs: "harness real-time e-markets",
+  },
+};
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <div>
+        <Post post={post} user={user} />
+        <br></br>
+        <Post post={post} user={user} details />
+      </div>
+    </div>
+    <Switch>
+      {/*<Route path="/users/:id/posts" component={UsersPost} />*/}
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/component/Post.js
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+
+const Post = ({ post, user, details }) => {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <p className="card-header-title">
+          <span className="is-uppercase is-size-3">
+            {post.title.slice(0, 1)}
+          </span>
+          {!details
+            ? post.title.slice(1, 50) + "..."
+            : post.title.slice(1, post.title.length)}
+        </p>
+      </div>
+      <p className="card-content">
+        <span className="is-uppercase is-size-3">{post.body.slice(0, 1)}</span>
+        {!details
+          ? post.body.slice(1, 50) + "..."
+          : post.body.slice(1, post.body.length)}
+      </p>
+      <div className="card-footer">
+        <p className="card-footer-item">
+          <NavLink to={"/users/" + user.id + "/posts"}>By {user.name}</NavLink>
+        </p>
+        {details ? <p className="card-footer-item">{user.email}</p> : null}
+      </div>
+    </div>
+  );
+};
+
+export default Post;
